b859bcdbd87ec630a0d934a2e92746bf28b1974f
Dagimawi sisay <dagmapply@gmail.com>
2022-07-07 21:07:56 +0300

    Added

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,34 +5,17 @@
  * Return: 0
 
  */
-=======
-*/
->>>>>>> refs/remotes/origin/master
 
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-
-	i = 0;
-
-	/**
-	* while (i < 10)
-	* {
-	*	putchar(i);
-	* }
-	*/
-=======
 	 i = 0;
-
 	 /*while (i < 10)*/
 	 /*{*/
 	 /*	putchar(i);*/
 	 /*}*/
-
->>>>>>> refs/remotes/origin/master
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
