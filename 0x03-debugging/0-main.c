470211df71590a81c2cc00da375a965380f57694
Dagim <dagmapply@gmail.com>
2023-12-21 14:05:02 +0300

    Main file edited

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
-
 * main - Test function for positive or negative
-
 * Return: 0
-
 */
 
 int main(void)
@@ -19,5 +16,5 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-	
+
 }
